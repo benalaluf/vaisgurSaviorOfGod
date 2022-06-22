de8909b89583cd94a50df519a91bee42ba5159b3
benalaluf <94129183+benalaluf@users.noreply.github.com>
2022-06-22 20:00:55 +0300

    misson 5 sucseed

--- src/lesson3/misson5.java
+++ src/lesson3/misson5.java
@@ -5,55 +5,36 @@ import java.util.Scanner;
 public class misson5 {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
-        int num1 = 1;
-        int num2 = 1;
-        int num3 = 1;
+        int num1 = 0;
+        int lastnum = 0;
         int sum = 0;
         double avg = 0;
         double counter = 0.0;
         int cd = 0;
 
 
-        while (num1 > 0 || num2 > 0 || num3 > 0) {
-            num1 = scn.nextInt();
-            if (num1 > 0) {
-                sum += num1;
-                counter++;
-                avg = sum / counter;
-            } else {
-                break;
+        while (num1 >= 0) {
+            for (int i = 0; i < 2; i++) {
+                lastnum = num1;
+                num1 = scn.nextInt();
+                if (num1 > 0) {
+                    sum += num1;
+                    counter++;
+                } else {
+                    break;
+                }
+
             }
-            num2 = scn.nextInt();
-            if (num2 > 0) {
-                sum += num2;
-                counter++;
-                avg = sum / counter;
-            } else {
-                break;
-            }
-            num3 = scn.nextInt();
-            if (num3 > 0) {
-                sum += num3;
-                counter++;
-                avg = sum / counter;
-            } else {
-                break;
-            }
-            if (num1 > avg) {
-                cd++;
-            }
-            if (num2 > avg) {
-                cd++;
-            }
-            if (num3 > avg) {
+            avg = sum / counter;
+            if (lastnum > avg) {
                 cd++;
             }
         }
-        if (counter==2){
-            if (num1>num2){
-                cd=1;
-            }
+        avg = sum / counter;
+        if (num1 > avg){
+            cd++;
         }
+
         System.out.println(cd);
     }
 }
